3d61931fc6123f34c4e2b8ec1f2948b4712ca909
yu-heejin <jkgt0521@naver.com>
2023-03-09 13:21:31 +0900

    [5일차] 다이내믹 임포트와 파일 다루기

--- node_study/3장/dynamic_import/dynamic.mjs
+++ node_study/3장/dynamic_import/dynamic.mjs
@@ -1,3 +1,20 @@
-const a = false;
+// const a = false;
 
-if (a)
+// if (a) {
+//     // import '../ecma_module/func.mjs';  오류 발생
+// }
+
+// console.log('성공');
+
+// es 모듈은 if문 안에서 import하는 것이 불가능하다.
+
+// 다음과 같이 수정할 수 있다.
+const a = true;
+
+if (a) {
+    const m1 = await import('../ecma_module/func.mjs');
+    console.log(m1);
+
+    const m2 = await import('../ecma_module/var.mjs');
+    console.log(m2);
+}
---
+++ node_study/3장/file/filename.mjs
@@ -0,0 +1,3 @@
+console.log(import.meta.url);
+console.log('__filename은 오류');
+console.log(__filename);
