0d91c9c421b6a57e88c918e47f22dad91e09acfe
yu-heejin <jkgt0521@naver.com>
2023-03-20 15:29:17 +0900

    [7일차] 노드 내장 모듈 - util

---
+++ node_study/3장/utils.js
@@ -0,0 +1,23 @@
+const util = require('util');
+const crypto = require('crypto');
+
+const dontUseMe = util.deprecate((x, y) => {
+    console.log(x + y);
+}, 'dontUseMe 함수는 deprecated 되었으니 더이상 사용하지 마세요!');
+// util.deprecate: 함수가 deprecated 처리되었음을 알려준다.
+// 첫번쨰 인수로 넣은 함수를 사용했을 때 경고 메세지가 출력된다.
+// 두번째 인수로 경고 메시지 내용을 넣으면 된다.
+// 함수가 조만간 사라지거나 변경될 때 알려줄 수 있어 유용하다.
+
+dontUseMe(1, 2);
+
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+// util.promisify: 콜백 패턴을 프로미스 패턴으로 바꾼다.
+// 바꿀 함수를 인수로 제공하면 된다.
+randomBytesPromise(64)
+.then((buf) => {
+    console.log(buf.toString('base64'));
+})
+.catch((error) => {
+    console.error(error);
+});
